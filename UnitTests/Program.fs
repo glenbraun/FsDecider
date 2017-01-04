9c8a5b40f009abf1d94d562b7085c270abe7acef
Glen Braun <glen@glenbraun.com>
2017-01-03 18:46:59 -0800

    Cleaned up list of test cases in unit test runner. Tried running in parallel and it worked perfectly.

--- UnitTests/Program.fs
+++ UnitTests/Program.fs
@@ -12,26 +12,20 @@ open Fuchu
 let tests = 
     testList "Primary Decider Actions" [
             testList "StartAndWaitForActivityTask" [
-                //(*
-                testCase "Completed" <|
-                    TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with One Completed Activity Task``
-                testCase "Canceled" <|
-                    TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with One Canceled Activity Task``
-                testCase "Failed" <|
-                    TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with One Failed Activity Task``
-                testCase "TimedOut" <|
-                    TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with One Timed Out Activity Task``
-                //*)
-                testCase "ScheduleFailed" <|
-                    TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with Activity Task Schedule Failure``
+                testCase "Completed"        <| TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with One Completed Activity Task``
+                testCase "Canceled"         <| TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with One Canceled Activity Task``
+                testCase "Failed"           <| TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with One Failed Activity Task``
+                testCase "TimedOut"         <| TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with One Timed Out Activity Task``
+                testCase "ScheduleFailed"   <| TestStartAndWaitForActivityTask.``Start And Wait For Activity Task with Activity Task Schedule Failure``
             ]
         ]
 
 [<EntryPoint>]
 let main argv = 
     TestConfiguration.GenerateOfflineHistory <- false
-    TestConfiguration.IsConnected <- true
+    TestConfiguration.IsConnected <- false
 
+    //runParallel tests |> ignore
     run tests |> ignore
     0
 
